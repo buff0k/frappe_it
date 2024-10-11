bf0e5e57f29e1d37cec2ad37a09d273e38a13112
buff0k <buff0k@gmail.com>
2024-10-11 19:30:16 +0000

    modified:   frappe_it/frappe_it/report/overspend_monthly_report/overspend_monthly_report.py Tweaks to Overage Report

--- frappe_it/frappe_it/report/overspend_monthly_report/overspend_monthly_report.py
+++ frappe_it/frappe_it/report/overspend_monthly_report/overspend_monthly_report.py
@@ -11,12 +11,12 @@ def execute(filters=None):
         frappe.throw("Please select a month to run the report.")
 
     columns = [
-        {"label": "Monthly Bill", "fieldname": "name", "fieldtype": "Link", "options": "Monthly Bill", "width": 120},
-        {"label": "SIM Card No", "fieldname": "sim_card_no", "fieldtype": "Link", "options": "Simcard Allocations", "width": 120},
-        {"label": "Telephone Number", "fieldname": "tel_no", "fieldtype": "Data", "width": 120},
-        {"label": "Employee", "fieldname": "employee", "fieldtype": "Data", "width": 120},
-        {"label": "Employee Name", "fieldname": "employee_name", "fieldtype": "Data", "width": 120},
-        {"label": "Overspend Amount", "fieldname": "overspend_amount", "fieldtype": "Currency", "width": 120}
+        {"label": "Monthly Bill", "fieldname": "name", "fieldtype": "Link", "options": "Monthly Bill", "width": 100},
+        {"label": "SIM Card No", "fieldname": "sim_card_no", "fieldtype": "Link", "options": "Simcard Allocations", "width": 100},
+        {"label": "Telephone Number", "fieldname": "tel_no", "fieldtype": "Data", "width": 130},
+        {"label": "Employee", "fieldname": "employee", "fieldtype": "Link", "options": "Employee", "width": 80},
+        {"label": "Employee Name", "fieldname": "employee_name", "fieldtype": "Data", "width": 150},
+        {"label": "Overspend Amount", "fieldname": "overspend_amount", "fieldtype": "Currency", "width": 100}
     ]
 
     conditions = "where b.overspend = 1"
@@ -29,12 +29,12 @@ def execute(filters=None):
             b.name, 
             b.sim_card_no, 
             s.tel_no,  -- Fetching tel_no from Simcard Allocations
-            b.employee, 
+            s.employee,  -- Fetching employee from Simcard Allocations
             e.employee_name, 
             b.overspend_amount
         from `tabMonthly Bill` b
-        left join `tabEmployee` e on b.employee = e.name
         left join `tabSimcard Allocations` s on b.sim_card_no = s.name  -- Join with Simcard Allocations
+        left join `tabEmployee` e on s.employee = e.name  -- Use employee from Simcard Allocations to fetch employee_name
         {conditions}
     """, filters, as_dict=True)
 
