d36d0c1ad0537f7815436e590d4e3e54a7251b29
buff0k <buff0k@gmail.com>
2024-12-06 09:44:43 +0000

    modified:   frappe_it/frappe_it/doctype/simcard_allocations/simcard_allocations.js 	modified:   frappe_it/frappe_it/doctype/simcard_allocations/simcard_allocations.json 	new file:   frappe_it/frappe_it/doctype/simcard_bills/__init__.py 	new file:   frappe_it/frappe_it/doctype/simcard_bills/simcard_bills.js 	new file:   frappe_it/frappe_it/doctype/simcard_bills/simcard_bills.json 	new file:   frappe_it/frappe_it/doctype/simcard_bills/simcard_bills.py 	new file:   frappe_it/frappe_it/doctype/simcard_bills/test_simcard_bills.py Added features to show linked simcard bills as well as allow simcards to be marked as active, replaced or lost

--- frappe_it/frappe_it/doctype/simcard_allocations/simcard_allocations.js
+++ frappe_it/frappe_it/doctype/simcard_allocations/simcard_allocations.js
@@ -1,8 +1,75 @@
 // Copyright (c) 2024, buff0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Simcard Allocations", {
-// 	refresh(frm) {
+frappe.ui.form.on('Simcard Allocations', {
+    onload: function (frm) {
+        frm.trigger('populate_child_table');
+    },
+    sim_no: function (frm) {
+        frm.trigger('populate_child_table');
+    },
+    employee: function (frm) {
+        if (frm.doc.employee) {
+            // Fetch employee details from the Employee DocType
+            frappe.db.get_doc('Employee', frm.doc.employee)
+                .then(employee_doc => {
+                    if (employee_doc) {
+                        // Update the fields with the fetched data
+                        frm.set_value('employee_name', employee_doc.employee_name);
+                        frm.set_value('branch', employee_doc.branch);
+                    }
+                });
+        } else {
+            // Clear the fields if employee is not selected
+            frm.set_value('employee_name', '');
+            frm.set_value('branch', '');
+        }
+    },
+    populate_child_table: function (frm) {
+        if (frm.doc.sim_no) {
+            frappe.call({
+                method: 'frappe.client.get_list',
+                args: {
+                    doctype: 'Monthly Bill',
+                    fields: ['name', 'total', 'overspend_amount'],
+                    filters: {
+                        sim_card_no: frm.doc.sim_no
+                    },
+                    limit_page_length: 0 // Fetch all matching records
+                },
+                callback: function (response) {
+                    if (response && response.message) {
+                        const bills = response.message;
 
-// 	},
-// });
+                        // Clear existing rows in the child table
+                        frm.clear_table('simcard_allocations');
+
+                        // Populate the child table with fetched data
+                        bills.forEach(bill => {
+                            let row = frm.add_child('simcard_allocations');
+                            row.monthly_bill = bill.name;
+                            row.total = bill.total;
+                            row.overspend_amount = bill.overspend_amount;
+                        });
+
+                        // Refresh the table to display the new data
+                        frm.refresh_field('simcard_allocations');
+
+                        // Mark the form as not dirty (prevents "Not Saved" message)
+                        frm.dirty = false;
+                    } else {
+                        // Clear the child table if no matching records are found
+                        frm.clear_table('simcard_allocations');
+                        frm.refresh_field('simcard_allocations');
+                        frm.dirty = false; // Mark the form as not dirty
+                    }
+                }
+            });
+        } else {
+            // Clear the table if sim_no is empty
+            frm.clear_table('simcard_allocations');
+            frm.refresh_field('simcard_allocations');
+            frm.dirty = false; // Mark the form as not dirty
+        }
+    }
+});
