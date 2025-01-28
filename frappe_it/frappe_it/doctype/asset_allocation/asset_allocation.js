4b1e6999dafd2c6d731a4078abfe10d4aa931029
Eben van Deventer <buff0k@gmail.com>
2025-01-28 12:25:50 +0200

    new file:   frappe_it/frappe_it/doctype/asset_allocation/__init__.py 	new file:   frappe_it/frappe_it/doctype/asset_allocation/asset_allocation.js 	new file:   frappe_it/frappe_it/doctype/asset_allocation/asset_allocation.json 	new file:   frappe_it/frappe_it/doctype/asset_allocation/asset_allocation.py 	new file:   frappe_it/frappe_it/doctype/asset_allocation/test_asset_allocation.py 	new file:   frappe_it/frappe_it/doctype/asset_allocation_table/__init__.py 	new file:   frappe_it/frappe_it/doctype/asset_allocation_table/asset_allocation_table.json 	new file:   frappe_it/frappe_it/doctype/asset_allocation_table/asset_allocation_table.py 	modified:   frappe_it/frappe_it/workspace/it/it.json Started Work on Asset Tracking doctype

---
+++ frappe_it/frappe_it/doctype/asset_allocation/asset_allocation.js
@@ -0,0 +1,63 @@
+// Copyright (c) 2025, buff0k and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Asset Allocation', {
+    employee_or_asset: function (frm) {
+        const selected_doctype = frm.doc.employee_or_asset;
+
+        if (!selected_doctype) {
+            frm.set_value('branch_or_location', null);
+            frm.set_value('site', null);
+            frm.set_value('employee_asset_name', null);
+            return;
+        }
+
+        // Set branch_or_location based on the selected doctype
+        if (selected_doctype === 'Employee') {
+            frm.set_value('branch_or_location', 'Branch');
+        } else if (selected_doctype === 'Asset') {
+            frm.set_value('branch_or_location', 'Location');
+        }
+
+        // Clear dependent fields until new data is fetched
+        frm.set_value('site', null);
+        frm.set_value('employee_asset_name', null);
+
+        // Trigger updates for site and employee_asset_name
+        frm.trigger('update_site_and_name');
+    },
+
+    allocated_to: function (frm) {
+        frm.trigger('update_site_and_name');
+    },
+
+    update_site_and_name: function (frm) {
+        const selected_doctype = frm.doc.employee_or_asset;
+        const docname = frm.doc.allocated_to;
+
+        if (!selected_doctype || !docname) {
+            frm.set_value('site', null);
+            frm.set_value('employee_asset_name', null);
+            return;
+        }
+
+        // Call the server script to fetch both the site and the employee_asset_name
+        frappe.call({
+            method: 'frappe_it.frappe_it.doctype.asset_allocation.asset_allocation.get_employee_or_asset_details',
+            args: {
+                doctype: selected_doctype,
+                docname: docname,
+            },
+            callback: function (response) {
+                if (response.message) {
+                    const { site, employee_asset_name } = response.message;
+                    frm.set_value('site', site || null);
+                    frm.set_value('employee_asset_name', employee_asset_name || null);
+                } else {
+                    frm.set_value('site', null);
+                    frm.set_value('employee_asset_name', null);
+                }
+            }
+        });
+    }
+});
---
+++ frappe_it/frappe_it/doctype/asset_allocation/asset_allocation.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2025, buff0k and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe import _
+
+
+class AssetAllocation(Document):
+	pass
+
+@frappe.whitelist()
+def get_employee_or_asset_details(doctype, docname):
+    if not doctype or not docname:
+        frappe.throw(_("Invalid parameters provided."))
+
+    try:
+        if doctype == "Employee":
+            # Fetch linked Branch and Employee Name
+            doc = frappe.get_doc("Employee", docname)
+            return {
+                "site": doc.branch if doc.branch else _("Branch not found"),
+                "employee_asset_name": doc.employee_name if doc.employee_name else _("Name not found"),
+            }
+        elif doctype == "Asset":
+            # Fetch linked Location and Item Name
+            doc = frappe.get_doc("Asset", docname)
+            return {
+                "site": doc.location if doc.location else _("Location not found"),
+                "employee_asset_name": doc.item_name if doc.item_name else _("Name not found"),
+            }
+        else:
+            frappe.throw(_("Invalid Doctype: {0}").format(doctype))
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), _("Error in get_employee_or_asset_details"))
+        frappe.throw(_("An error occurred while fetching the document data: {0}").format(str(e)))
