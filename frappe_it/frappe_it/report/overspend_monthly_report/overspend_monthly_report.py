d04903f0666c66224ba298974ac1c714572ac680
Isambane Group Domain <isambane@isex1.isambane.co.za>
2025-07-08 09:33:34 +0200

    modified:   frappe_it/fixtures/custom_docperm.json 	modified:   frappe_it/frappe_it/report/overspend_monthly_report/overspend_monthly_report.py 	modified:   frappe_it/hooks.py Moved DocLinks to after_migrate Fixed Overspend Report for amended Simcard Allocations

--- frappe_it/frappe_it/report/overspend_monthly_report/overspend_monthly_report.py
+++ frappe_it/frappe_it/report/overspend_monthly_report/overspend_monthly_report.py
@@ -14,7 +14,7 @@ def execute(filters=None):
         {"label": "Monthly Bill", "fieldname": "name", "fieldtype": "Link", "options": "Monthly Bill", "width": 100},
         {"label": "SIM Card No", "fieldname": "sim_card_no", "fieldtype": "Link", "options": "Simcard Allocations", "width": 205},
         {"label": "Telephone Number", "fieldname": "tel_no", "fieldtype": "Data", "width": 145},
-        {"label": "Employee", "fieldname": "employee", "fieldtype": "Link", "options": "Employee", "width": 120},
+        {"label": "Allocated To", "fieldname": "allocated_to", "fieldtype": "Link", "options": "Employee", "width": 120},
         {"label": "Employee Name", "fieldname": "employee_name", "fieldtype": "Data", "width": 150},
         {"label": "Notes", "fieldname": "notes", "fieldtype": "Small Text", "width": 250},
         {"label": "Overspend Amount", "fieldname": "overspend_amount", "fieldtype": "Data", "width": 100}
@@ -30,13 +30,13 @@ def execute(filters=None):
             b.name, 
             b.sim_card_no, 
             s.tel_no,  -- Fetching tel_no from Simcard Allocations
-            s.employee,  -- Fetching employee from Simcard Allocations
+            s.allocated_to,  -- Fetching allocated_to from Simcard Allocations
             s.notes, -- Fetching notes from Simcard Allocations
             e.employee_name, 
             b.overspend_amount
         from `tabMonthly Bill` b
         left join `tabSimcard Allocations` s on b.sim_card_no = s.name  -- Join with Simcard Allocations
-        left join `tabEmployee` e on s.employee = e.name  -- Use employee from Simcard Allocations to fetch employee_name
+        left join `tabEmployee` e on s.allocated_to = e.name  -- Use employee from Simcard Allocations to fetch employee_name
         {conditions}
     """, filters, as_dict=True)
 
